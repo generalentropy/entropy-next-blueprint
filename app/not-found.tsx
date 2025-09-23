187a611031f25e85f3f7eb07acf2f6137b879ebc
Eddy Nicolle <entropie@tuta.io>
2025-09-23 19:51:44 +0200

    feat:add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <p className="text-8xl font-black">404</p>
+        <p className="text-muted-foreground text-3xl font-medium tracking-tight">
+          Not found
+        </p>
+      </div>
+    </div>
+  );
+}
